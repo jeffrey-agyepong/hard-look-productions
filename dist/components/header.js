406207b9cf759cd241063b61c4ebad834a6175e6
Jeffrey <agyepong99@gmail.com>
2021-05-27 01:51:33 +0000

    hero

--- dist/components/header.js
+++ dist/components/header.js
@@ -5,42 +5,82 @@ class Header extends HTMLElement {
 
   connectedCallback() {
     this.innerHTML = `
-<header>
-  <div class="navbar-fixed">
-    <nav class="nav-center black" role="navigation" id="header">
-      <div class="nav-wrapper container w-menu">
-        <a href="https://beyondthegravemusic.com" class="brand-logo">HL Productions</a>
+      <header>
+        <div class="transparent">
+          <nav
+            class="nav-exteneded transparent z-depth-0"
+            role="navigation"
+            id="header"
+          >
+            <div class="nav-wrapper container w-menu">
+              <a
+                href="https://beyondthegravemusic.com"
+                class="brand-logo center"
+              >
+                HL Productions
+              </a>
 
-        <ul class="right hide-on-med-and-down">
-          <li> <a href="index.html">About</a></li>
-          <li> <a href="index.html">Services</a></li>
-          <li><a href="">Projects</a></li>
-          <li> <a href="index.html">Testimonials</a></li>
-          <li><a href="">Contact</a></li>
+              <ul id="nav-mobile" class="sidenav black center sticky">
+                <li>
+                  <a href="index.html" class="white-text">
+                    About
+                  </a>
+                </li>
+                <li>
+                  <a href="index.html" class="white-text">
+                    Services
+                  </a>
+                </li>
+                <li>
+                  <a href="" class="white-text">
+                    Projects
+                  </a>
+                </li>
+                <li>
+                  <a href="" class="white-text">
+                    Testimonials
+                  </a>
+                </li>
+                <li>
+                  <a href="" class="white-text">
+                    Contact
+                  </a>
+                </li>
+              </ul>
+              <a
+                href="#"
+                data-target="nav-mobile"
+                class="sidenav-trigger header_toggle"
+              >
+                <span></span>
+                <span></span>
+                <span></span>
+              </a>
+            </div>
+            <div class="nav-content">
+              <ul class="tabs tabs-transparent center hide-on-med-and-down">
+                <li class="tab">
 
-        </ul>
-
-        <ul id="nav-mobile" class="sidenav black center">
-          <li><a href="index.html" class="white-text">About</a></li>
-          <li><a href="index.html" class="white-text">Services</a></li>
-          <li><a href="" class="white-text">Projects</a></li>
-          <li><a href="" class="white-text">Testimonials</a></li>
-          <li><a href="" class="white-text">Contact</a></li>
-
-          <!--<li><a href="https://store.beyondthegravemusic.com" class="white-text">Store</a></li>-->
-
-        </ul>
-        <a href="#" data-target="nav-mobile" class="sidenav-trigger header_toggle">
-          <span></span>
-          <span></span>
-          <span></span>
-        </a>
-      </div>
-    </nav>
-  </div>
-</header>
-
-`;
+                  <a href="index.html">About</a>
+                </li>
+                <li class="tab">
+                  <a href="index.html">Services</a>
+                </li>
+                <li class="tab">
+                  <a href="">Projects</a>
+                </li>
+                <li class="tab">
+                  <a href="index.html">Testimonials</a>
+                </li>
+                <li class="tab">
+                  <a href="">Contact</a>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </div>
+      </header>
+  `;
   }
 }
 customElements.define("header-component", Header);
